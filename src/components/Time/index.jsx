f35b1f92237519772fabbf097a3ab6df44cdba3b
suyi <695708197@qq.com>
2019-04-02 16:22:28 +0800

    时钟添加  头部层级修改

--- src/components/Time/index.jsx
+++ src/components/Time/index.jsx
@@ -1,52 +1,193 @@
 import './index.scss';
 import React from 'react';
 
+/**
+ * 0 123456
+ * 1 23
+ * 2 12457
+ * 3 12347
+ * 4 2367
+ * 5 1367
+ * 6 134567
+ * 7 123
+ * 8 1234567
+ * 9 123467
+ */
+const ZERO = '123456';
+const ONE = '23';
+const TWO = '12457';
+const THREE = '12347';
+const FOUR = '2367';
+const FIVE = '13467';
+const SIX = '134567';
+const SEVEN = '123';
+const EIGHT = '1234567';
+const NINE = '123467';
+
 class TimeBox extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { date: new Date(), weather: null };
+    this.state = { times: ['0', '0', '0', '0', '0', '0'] };
   }
 
   componentDidMount() {
-    this.timerID = setInterval(
-      () => this.tick(),
-      1000
-    );
+    this.timerID = setInterval(() => this.getTime(), 1000);
   }
 
   componentWillMount() {
     clearInterval(this.timerID);
   }
 
-  tick() {
-    this.setState({
-      date: new Date()
+  getTime = () => {
+    const DATE = new Date();
+    let HOURS = DATE.getHours();
+    if (HOURS < 10) {
+      HOURS = `0${HOURS}`;
+    }
+    let MINUTES = DATE.getMinutes();
+    if (MINUTES < 10) {
+      MINUTES = `0${MINUTES}`;
+    }
+    let SECONDS = DATE.getSeconds();
+    if (SECONDS < 10) {
+      SECONDS = `0${SECONDS}`;
+    }
+    const times = `${HOURS}${MINUTES}${SECONDS}`;
+    const arr = times.split('');
+    const timeArr = [];
+    arr.forEach(v => {
+      timeArr.push(this.changeNumber(v));
     });
-  }
+    this.setState({
+      times: timeArr,
+    });
+  };
 
-  getCurrentHour() {
-    let hour = this.state.date.getHours();
-    hour < 10 ? hour = "0" + hour : null;
-    return hour;
-  }
+  changeNumber = (random = 0) => {
+    random -= 0;
+    let number = 0;
+    switch (random) {
+      case 0:
+        number = ZERO;
+        break;
+      case 1:
+        number = ONE;
+        break;
+      case 2:
+        number = TWO;
+        break;
+      case 3:
+        number = THREE;
+        break;
+      case 4:
+        number = FOUR;
+        break;
+      case 5:
+        number = FIVE;
+        break;
+      case 6:
+        number = SIX;
+        break;
+      case 7:
+        number = SEVEN;
+        break;
+      case 8:
+        number = EIGHT;
+        break;
+      case 9:
+        number = NINE;
+        break;
+      default:
+        break;
+    }
+    return number;
+  };
 
-  getCurrentMinute() {
-    let minute = this.state.date.getMinutes();
-    minute < 10 ? minute = "0" + minute : null;
-    return minute;
-  }
+  // tick() {
+  //   this.setState({
+  //     date: new Date(),
+  //   });
+  // }
 
-  getCurrentSecond() {
-    let seconde = this.state.date.getSeconds();
-    seconde < 10 ? seconde = "0" + seconde : null;
-    return seconde;
-  }
+  // getCurrentHour() {
+  //   let hour = this.state.date.getHours();
+  //   hour < 10 ? hour = `0${hour}` : null;
+  //   return hour;
+  // }
+
+  // getCurrentMinute() {
+  //   let minute = this.state.date.getMinutes();
+  //   minute < 10 ? minute = `0${minute}` : null;
+  //   return minute;
+  // }
+
+  // getCurrentSecond() {
+  //   let seconde = this.state.date.getSeconds();
+  //   seconde < 10 ? seconde = `0${seconde}` : null;
+  //   return seconde;
+  // }
+
   render() {
+    const { times } = this.state;
+    const { color = '#fff' } = this.props;
     return (
-      <div className="clock-div">
-        {this.getCurrentHour().toString()}:
-        {this.getCurrentMinute().toString()}:
-        {this.getCurrentSecond().toString()}
+      <div className="number">
+        <div className="digit minutes">
+          {[1, 2, 3, 4, 5, 6, 7].map(v => (
+            <div
+              className={`segment ${times[0].includes(v) ? 'on' : ''}`}
+              style={{ backgroundColor: color }}
+            />
+          ))}
+        </div>
+        <div className="digit minutes">
+          {[1, 2, 3, 4, 5, 6, 7].map(v => (
+            <div
+              className={`segment ${times[1].includes(v) ? 'on' : ''}`}
+              style={{ backgroundColor: color }}
+            />
+          ))}
+        </div>
+        <div className="circle">
+          <div className="circle-boom" style={{ backgroundColor: color }} />
+          <div className="circle-boom" style={{ backgroundColor: color }} />
+        </div>
+        <div className="digit minutes">
+          {[1, 2, 3, 4, 5, 6, 7].map(v => (
+            <div
+              className={`segment ${times[2].includes(v) ? 'on' : ''}`}
+              style={{ backgroundColor: color }}
+            />
+          ))}
+        </div>
+        <div className="digit minutes">
+          {[1, 2, 3, 4, 5, 6, 7].map(v => (
+            <div
+              className={`segment ${times[3].includes(v) ? 'on' : ''}`}
+              style={{ backgroundColor: color }}
+            />
+          ))}
+        </div>
+        <div className="circle">
+          <div className="circle-boom" style={{ backgroundColor: color }} />
+          <div className="circle-boom" style={{ backgroundColor: color }} />
+        </div>
+        <div className="digit minutes">
+          {[1, 2, 3, 4, 5, 6, 7].map(v => (
+            <div
+              className={`segment ${times[4].includes(v) ? 'on' : ''}`}
+              style={{ backgroundColor: color }}
+            />
+          ))}
+        </div>
+        <div className="digit minutes">
+          {[1, 2, 3, 4, 5, 6, 7].map(v => (
+            <div
+              className={`segment ${times[5].includes(v) ? 'on' : ''}`}
+              style={{ backgroundColor: color }}
+            />
+          ))}
+        </div>
       </div>
     );
   }
